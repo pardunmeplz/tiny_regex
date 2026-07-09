14c91337d1589ed354bea2732866a9d9b2d7a521
Parth Dundh <dundh.parth@gmail.com>
2026-07-10 01:42:56 +0530

    NFA implementations for Thompson's construction

--- internal/regex/nfa.go
+++ internal/regex/nfa.go
@@ -1,6 +1,7 @@
 package regex
 
 type Transition struct {
+	// its a poniter because epsilon transitions will have In as nil
 	In  *rune
 	Out *State
 }
@@ -10,6 +11,33 @@ type State struct {
 }
 
 type NFA struct {
-	Start  []*State
-	Accept []*State
+	Start  *State
+	Accept *State
+}
+
+func literalNfa(value rune) NFA {
+	out := NFA{&State{}, &State{}}
+	out.Start.Transitions = []Transition{{&value, out.Accept}}
+	return out
+}
+
+func concatinationNfa(left NFA, right NFA) NFA {
+	out := NFA{left.Start, right.Accept}
+	left.Accept.Transitions = append(left.Accept.Transitions, Transition{nil, right.Start})
+	return out
+}
+
+func alterationNfa(left NFA, right NFA) NFA {
+	out := NFA{&State{[]Transition{{nil, left.Start}, {nil, right.Start}}}, &State{}}
+	left.Accept.Transitions = append(left.Accept.Transitions, Transition{nil, out.Accept})
+	right.Accept.Transitions = append(right.Accept.Transitions, Transition{nil, out.Accept})
+	return out
+}
+
+func repeatNfa(repeat NFA) NFA {
+	out := NFA{&State{}, &State{}}
+	out.Start.Transitions = []Transition{{nil, out.Accept}, {nil, repeat.Start}}
+	repeat.Accept.Transitions = append(repeat.Accept.Transitions, Transition{nil, repeat.Start}, Transition{nil, out.Accept})
+
+	return out
 }
