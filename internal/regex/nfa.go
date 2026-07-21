2787e997a67f61702d6b9f7d927baf1f8c9d69d6
Parth Dundh <dundh.parth@gmail.com>
2026-07-22 01:41:54 +0530

    Evaluating NFA product by graph traversal

--- internal/regex/nfa.go
+++ internal/regex/nfa.go
@@ -1,13 +1,10 @@
 package regex
 
-type Transition struct {
-	// its a poniter because epsilon transitions will have In as nil
-	In  *rune
-	Out *State
-}
-
 type State struct {
-	Transitions []Transition
+	// the reason we don't need a list of state is that one transition leading to multiple states is always handled by epsilons
+	// in case o rune leads to multiple states, you end up having it transition to one intermediate state with many epsilons transitions instead
+	Transitions map[rune]*State
+	Epsilons    map[*State]struct{}
 }
 
 type NFA struct {
@@ -15,29 +12,37 @@ type NFA struct {
 	Accept *State
 }
 
+type Visited map[[2]*State]struct{}
+
 func literalNfa(value rune) NFA {
 	out := NFA{&State{}, &State{}}
-	out.Start.Transitions = []Transition{{&value, out.Accept}}
+	out.Start.Transitions[value] = out.Accept
 	return out
 }
 
 func concatinationNfa(left NFA, right NFA) NFA {
 	out := NFA{left.Start, right.Accept}
-	left.Accept.Transitions = append(left.Accept.Transitions, Transition{nil, right.Start})
+	left.Accept.Epsilons[right.Start] = struct{}{}
 	return out
 }
 
 func alterationNfa(left NFA, right NFA) NFA {
-	out := NFA{&State{[]Transition{{nil, left.Start}, {nil, right.Start}}}, &State{}}
-	left.Accept.Transitions = append(left.Accept.Transitions, Transition{nil, out.Accept})
-	right.Accept.Transitions = append(right.Accept.Transitions, Transition{nil, out.Accept})
+	out := NFA{&State{}, &State{}}
+	out.Start.Epsilons[left.Start] = struct{}{}
+	out.Start.Epsilons[right.Start] = struct{}{}
+
+	left.Accept.Epsilons[out.Accept] = struct{}{}
+	right.Accept.Epsilons[out.Accept] = struct{}{}
 	return out
 }
 
 func repeatNfa(repeat NFA) NFA {
 	out := NFA{&State{}, &State{}}
-	out.Start.Transitions = []Transition{{nil, out.Accept}, {nil, repeat.Start}}
-	repeat.Accept.Transitions = append(repeat.Accept.Transitions, Transition{nil, repeat.Start}, Transition{nil, out.Accept})
+	out.Start.Epsilons[out.Accept] = struct{}{}
+	out.Start.Epsilons[repeat.Start] = struct{}{}
+
+	repeat.Accept.Epsilons[repeat.Start] = struct{}{}
+	repeat.Accept.Epsilons[out.Accept] = struct{}{}
 
 	return out
 }
@@ -56,4 +61,60 @@ func thompsonConstruction(node Node) NFA {
 		return thompsonConstruction(*node.Left)
 	}
 	return NFA{}
+}
+
+func evaluateNfaProduct(a NFA, b NFA) bool {
+	return evaluateProduct(a.Start, b.Start, a.Accept, b.Accept, Visited{})
+}
+
+func evaluateProduct(A *State, B *State, winA *State, winB *State, visited Visited) bool {
+
+	// visited check to avoid infinite recursions
+	if _, has := visited[[2]*State{A, B}]; has {
+		return false
+	}
+	visited[[2]*State{A, B}] = struct{}{}
+
+	aStates := flattenEpsilonTransitions(A)
+	bStates := flattenEpsilonTransitions(B)
+
+	// winCheck
+	if _, successA := aStates[winA]; successA {
+		if _, successB := bStates[winB]; successB {
+			return true
+		}
+	}
+
+	// find win further down the tree
+	for nextA := range aStates {
+		for nextB := range bStates {
+			for t := range nextA.Transitions {
+				if _, has := nextB.Transitions[t]; has && evaluateProduct(nextA.Transitions[t], nextB.Transitions[t], winA, winB, visited) {
+					return true
+				}
+			}
+
+		}
+	}
+
+	// no wins found
+	return false
+}
+
+func flattenEpsilonTransitions(A *State) map[*State]struct{} {
+	out := map[*State]struct{}{}
+	out[A] = struct{}{}
+	for {
+		outLen := len(out)
+		for s := range out {
+			for t := range s.Epsilons {
+				out[t] = struct{}{}
+			}
+		}
+		if outLen == len(out) {
+			break
+		}
+	}
+
+	return out
 }
