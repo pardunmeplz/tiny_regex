e6f7185d89dd42466d0f1c54a94749eaf339d144
Parth Dundh <dundh.parth@gmail.com>
2026-07-23 00:06:24 +0530

    expose function as a teminal command

--- cmd/matcher/main.go
+++ cmd/matcher/main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	rx "regex/internal/regexEval"
 )
 
 func main() {
-	fmt.Print(rx.HasUnion("ab", "b|ab*"))
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "usage: %s <regex-a> <regex-b>\n", os.Args[0])
+		os.Exit(2)
+	}
+
+	ok, errs := rx.HasUnion(os.Args[1], os.Args[2])
+	if len(errs) > 0 {
+		for _, e := range errs {
+			fmt.Fprintln(os.Stderr, e)
+		}
+		os.Exit(1)
+	}
+
+	if ok {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
 }
